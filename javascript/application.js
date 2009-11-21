2c64f254f010b54fdbf4ad1850a424ff0de506c3
scott <scott@40c6d7c3-f295-467c-8717-68c1440881de>
2009-11-21 22:26:14 +0000

    Initial ship builder files Radio buttons for ship class are too much
    git-svn-id: svn://192.168.1.100/trunk@144 40c6d7c3-f295-467c-8717-68c1440881de

---
+++ javascript/application.js
@@ -0,0 +1,106 @@
+(function()
+{
+	//instantiate: true to create a single instance immediately
+	var classes =
+	[
+		{ name: 'Creator' },
+		{ name: 'Item' },
+		{ name: 'List' },
+		{ name: 'Region', superclass: 'Item' },
+		{ name: 'User', superclass: 'Item' },
+		{ name: 'Schedule', instantiate: true },
+		{ name: 'Site', superclass: 'Item' },
+		{ name: 'SiteCreator', superclass: 'Creator', instantiate: true },
+		{ name: 'TestConnection', instantiate: true }
+	];
+	
+	var instances =
+	[
+		{ name: 'region_creator', class_name: 'Creator' },
+		{ name: 'region_list', class_name: 'List' },
+		{ name: 'site_list', class_name: 'List' },
+		{ name: 'user_creator', class_name: 'Creator' },
+		{ name: 'user_list', class_name: 'List' }
+	];
+	
+	//event handlers
+	function start()
+	{
+		classes.each(class_builder);
+		instances.each(instance_builder);
+	}
+	
+	function class_builder(class_spec)
+	{
+		var model;
+		var view;
+		var controller;
+		
+		try
+		{
+			model = eval(class_spec.name + 'Model');
+			view = eval(class_spec.name + 'View');
+			controller = eval(class_spec.name + 'Controller');
+		}
+		catch(exception)
+		{
+			//component(s) of the class not here, moving on...
+		}
+		
+		//check first for the presence of the components; this allows each class to built optionally, ignored by pages that don't need it, simply by including or excluding those script tags in the header 
+		if (model && view && controller)
+		{
+			if (class_spec.superclass)
+			{
+				if (window[class_spec.superclass])
+				{
+					window[class_spec.name] = Class.create(window[class_spec.superclass], model, view, controller);
+				}
+				else
+				{
+					throw 'Missing superclass ' + class_spec.superclass + ' (required by ' + class_spec.name + ')';
+				}
+			}
+			else
+			{
+				window[class_spec.name] = Class.create(model, view, controller);
+			}
+
+			if (class_spec.instantiate)
+			{
+				return instance_builder(
+				{
+					class_name: class_spec.name,
+					options: class_spec.options || {} //instance_builder does not build if !options
+				});
+			}
+		}
+	}
+	
+	function instance_builder(instance_spec)
+	{
+		if (window[instance_spec.class_name])
+		{
+			//aside from classes (instantiate), instance_builder triggers when 'name'_options is declared (preferably in a partial) for the instance
+			var options = instance_spec.options;
+			if (!options && instance_spec.name)
+			{
+				try
+				{
+					options = eval(instance_spec.name + '_options');
+				}
+				catch(exception)
+				{
+					//options for the instance not here, moving on...
+				}
+			}
+			if (options)
+			{
+				var instance = new window[instance_spec.class_name](options);
+				return instance;
+			}
+		}
+	}
+
+	document.observe("dom:loaded", start);	
+})();
