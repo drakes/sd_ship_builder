e0ae4016e137c99718ad76b756fd794a294b8164
scott <scott@40c6d7c3-f295-467c-8717-68c1440881de>
2009-11-21 22:30:56 +0000

    Added medium1pfighter data
    git-svn-id: svn://192.168.1.100/sd_ship_builder/trunk@153 40c6d7c3-f295-467c-8717-68c1440881de

--- javascript/construction_template_data.js
+++ javascript/construction_template_data.js
@@ -86,6 +86,96 @@ var ConstructionTemplateData =
 	
 	medium1pfighter:
 	{
+		200:
+		{
+			crew: 1,
+			slots: 18,
+			max_torps: 10,
+			hit_boxes: 20,
+			drive:
+			{
+				10: { cost: 4, slots: 2 },
+				11: { cost: 4, slots: 2 },
+				12: { cost: 5, slots: 2 },
+				13: { cost: 5, slots: 3 },
+				14: { cost: 6, slots: 3 },
+				15: { cost: 6, slots: 4 },
+				16: { cost: 6, slots: 4 },
+				17: { cost: 7, slots: 5 },
+				18: { cost: 11, slots: 8 },
+				19: { cost: 12, slots: 9 },
+				20: { cost: 12, slots: 10 }
+			},
+			defense:
+			{
+				6: { cost: 2, slots: 1 },
+				7: { cost: 3, slots: 1 },
+				8: { cost: 3, slots: 1 },
+				9: { cost: 4, slots: 1 },
+				10: { cost: 4, slots: 2 },
+				11: { cost: 4, slots: 2 },
+				12: { cost: 5, slots: 2 },
+				13: { cost: 5, slots: 3 },
+				14: { cost: 6, slots: 3 },
+				15: { cost: 9, slots: 6 },
+				16: { cost: 10, slots: 7 },
+				17: { cost: 11, slots: 8 }
+			},
+			damage_reduction:
+			{
+				0: { cost: 0, slots: 0 },
+				1: { cost: 1, slots: 2 },
+				2: { cost: 2, slots: 4 },
+				3: { cost: 3, slots: 6 },
+				4: { cost: 4, slots: 8 },
+				5: { cost: 5, slots: 10 }
+			}
+		},
+		300:
+		{
+			crew: 1,
+			slots: 28,
+			max_torps: 10,
+			hit_boxes: 30,
+			drive:
+			{
+				10: { cost: 6, slots: 2 },
+				11: { cost: 7, slots: 3 },
+				12: { cost: 7, slots: 3 },
+				13: { cost: 8, slots: 4 },
+				14: { cost: 8, slots: 5 },
+				15: { cost: 9, slots: 5 },
+				16: { cost: 10, slots: 6 },
+				17: { cost: 15, slots: 11 },
+				18: { cost: 17, slots: 12 },
+				19: { cost: 17, slots: 14 },
+				20: { cost: 18, slots: 15 }
+			},
+			defense:
+			{
+				6: { cost: 4, slots: 1 },
+				7: { cost: 4, slots: 1 },
+				8: { cost: 5, slots: 2 },
+				9: { cost: 5, slots: 2 },
+				10: { cost: 6, slots: 2 },
+				11: { cost: 7, slots: 3 },
+				12: { cost: 7, slots: 3 },
+				13: { cost: 8, slots: 4 },
+				14: { cost: 12, slots: 8 },
+				15: { cost: 14, slots: 8 },
+				16: { cost: 15, slots: 9 },
+				17: { cost: 15, slots: 11 }
+			},
+			damage_reduction:
+			{
+				0: { cost: 0, slots: 0 },
+				1: { cost: 2, slots: 3 },
+				2: { cost: 3, slots: 6 },
+				3: { cost: 5, slots: 9 },
+				4: { cost: 6, slots: 12 },
+				5: { cost: 8, slots: 15 }
+			}
+		}
 	},
 
 	heavy1pfighter:
