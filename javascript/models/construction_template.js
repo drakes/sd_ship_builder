936fd2f4e3ded8fd578b854142605b5d70ad0664
scott <scott@40c6d7c3-f295-467c-8717-68c1440881de>
2009-11-21 22:33:29 +0000

    Shortened names, now JavaScript files rely on directories for context
    git-svn-id: svn://192.168.1.100/sd_ship_builder/trunk@158 40c6d7c3-f295-467c-8717-68c1440881de

---
+++ javascript/models/construction_template.js
@@ -0,0 +1,51 @@
+var ConstructionTemplateModel =
+{
+	initialize: function(options)
+	{
+		this.options =
+		{
+			//selectors and CSS
+			ship_class_id: 'ship_class',
+			tons_id: 'tons',
+			hint_class: 'hint',
+
+			//events
+			template_changed_event: 'template:changed',
+
+			//templates
+			data: null
+		};
+		Object.extend(this.options, options);
+
+		this.connect_event_handlers();
+	},
+
+	get_ship_template: function(ship_class, tons)
+	{
+		if (!ship_class)
+		{
+			ship_class = this.get_ship_class();
+		}
+		if (!tons)
+		{
+			tons = this.get_tons();
+		}
+
+		return this.options.data[ship_class][tons];
+	},
+
+	get_tons_options: function(ship_class)
+	{
+		return $H(this.options.data[ship_class]).keys();
+	},
+
+	get_ship_class: function()
+	{
+		return $F(this.options.ship_class_id);
+	},
+	
+	get_tons: function()
+	{
+		return $F(this.options.tons_id);
+	}
+};
